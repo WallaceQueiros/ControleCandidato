5b45d6b9cf651fc8d849d31b43afd5aad44656e8
Wallace Queiros <wallacequeiros@hotmail.com>
2024-05-14 15:49:16 -0300

    criação do metodo Analisar Candidatura e as condições

--- controle-candidato/src/candidatura/ProcessoSeletivo.java
+++ controle-candidato/src/candidatura/ProcessoSeletivo.java
@@ -2,6 +2,18 @@ package candidatura;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Processo Seletivo");
+ 
+    }
+
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO");
+        }else if (salarioBase == salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+        }else{
+            System.out.println("AGUARDADNO O RESULTADI DIS DEMAIS CANDIDATOS");
+        }
     }
 }
